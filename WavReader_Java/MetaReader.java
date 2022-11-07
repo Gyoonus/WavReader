d70abb437fffadb8f2520f59d5c01ba673b22011
gyoonus <kim.gs@kt.com>
2022-11-07 15:23:04 +0900

    update format

--- WavReader_Java/MetaReader.java
+++ WavReader_Java/MetaReader.java
@@ -9,16 +9,30 @@ import java.io.IOException;
  */
 public class MetaReader {
     public static void main(String[] args) {
+        /*
         if (args.length == 0) {
             System.out.println("Illegal command line arguments.\n" +
                     "Usage MetaReader\n\t MetaReader <source file path>");
             return;
         }
+
+        */
         WavHeader wavHeader;
         try {
-            WavHeaderReader wavHeaderReader = new WavHeaderReader(args[0]);
+            //WavHeaderReader wavHeaderReader = new WavHeaderReader(args[0]);
+            WavHeaderReader wavHeaderReader = new WavHeaderReader("/home/gyoo/test1234.wav");
             wavHeader = wavHeaderReader.read();
-            System.out.println(wavHeader.toString());
+            String subchunk = new String(wavHeader.getSubChunk2ID());
+
+            if(subchunk.equals("data") || subchunk.equals("DATA"))
+            {
+                System.out.println("supported File Format");
+            }
+            else
+            {
+                System.out.println("Unsupported File Format");
+            }
+
         } catch (FileNotFoundException e) {
             System.out.println("Error: File " + args[0] + " not found!");
         } catch (IOException e) {
